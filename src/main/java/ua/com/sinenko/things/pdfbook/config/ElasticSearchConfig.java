99d12cecb97769dc83277bad440c209fd1d9f5e7
andy.sinenko <andy.sinenko@gmail.com>
2025-09-30 18:36:38 +0300

    all PG and ElSearch settings has moved to environment variables

--- src/main/java/ua/com/sinenko/things/pdfbook/config/ElasticSearchConfig.java
+++ src/main/java/ua/com/sinenko/things/pdfbook/config/ElasticSearchConfig.java
@@ -1,5 +1,8 @@
 package ua.com.sinenko.things.pdfbook.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
@@ -16,22 +19,39 @@ import java.time.Duration;
 @Profile("stage")
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
 
+    @Value("${ELASTICSEARCH_KEY}")
+    private String keyFile;
+
+    @Value("${ELASTICSEARCH_KEY_PASSWORD}")
+    private String keyFilePassword;
+
+    @Value("${ELASTICSEARCH_HOST}")
+    private String host;
+
+    @Value("${ELASTICSEARCH_PORT}")
+    private String port;
+
+    @Value("${ELASTICSEARCH_USERNAME}")
+    private String username;
+
+    @Value("${ELASTICSEARCH_PASSWORD}")
+    private String password;
 
     @Override
     public ClientConfiguration clientConfiguration() {
+
         try {
             KeyStore trustStore = KeyStore.getInstance("JKS");
-            trustStore.load(new FileInputStream("./elasticsearch/elasticsearch-ca.jks"), "123456".toCharArray());
+            trustStore.load(new FileInputStream(keyFile), keyFilePassword.toCharArray());
             TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
             tmf.init(trustStore);
             SSLContext sslContext = SSLContext.getInstance("TLS");
             sslContext.init(null, tmf.getTrustManagers(), null);
 
-
             return ClientConfiguration.builder()
-                    .connectedTo("localhost:9200")
+                    .connectedTo(host + ":" + port)
                     .usingSsl(sslContext)
-                    .withBasicAuth("things_service", "things12345")
+                    .withBasicAuth(username, password)
                     .withConnectTimeout(Duration.ofSeconds(5))
                     .withSocketTimeout(Duration.ofSeconds(3))
                     .build();
