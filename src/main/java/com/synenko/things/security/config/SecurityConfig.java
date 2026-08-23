a5684b231d9bc0e8a0249eb84d709cd2378075e9
andy.sinenko <andy.sinenko@gmail.com>
2026-08-23 20:34:34 +0300

    fix(things): added commands for server install

--- src/main/java/com/synenko/things/security/config/SecurityConfig.java
+++ src/main/java/com/synenko/things/security/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of("http://localhost:3000"));
+                    config.setAllowedOrigins(List.of("http://localhost:3000", "https://things.synenko.com"));
                     config.setAllowedMethods(List.of("*"));
                     config.setAllowCredentials(true);
                     config.setAllowedHeaders(List.of("*"));
